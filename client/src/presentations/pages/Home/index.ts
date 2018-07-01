b5387e4a3fff90848f05c5bf44ded53750dc9890
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-07-02 07:11:17 +0900

    Try to use anonymously sign in

--- client/src/presentations/pages/Home/index.ts
+++ client/src/presentations/pages/Home/index.ts
@@ -1,5 +1,6 @@
 import { ILayout } from 'presentations/application/Layout';
 import { sampleService } from 'presentations/services/sampleService';
+import { auth } from 'presentations/utils/auth';
 import { logger } from 'presentations/utils/logger';
 import { config } from 'secrets/config';
 
@@ -10,6 +11,13 @@ export interface IHomePage extends ILayout {
 window.addEventListener('DOMContentLoaded', () => {
   logger.info(`Start HomePage at ${new Date().toString()}.`);
 
-  sampleService.public();
-  sampleService.private();
+  if (!auth.isSignin()) {
+    auth.signupNewUser().then(() => {
+      sampleService.public();
+      sampleService.private();
+    });
+  } else {
+    sampleService.public();
+    sampleService.private();
+  }
 });
