c1025ea63b762e02230488b49c342842d835a7ac
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-07-07 13:06:55 +0900

    Update main.go

--- api-go/src/app/main.go
+++ api-go/src/app/main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"html"
+	"encoding/json"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+type Resource struct {
+	Message string `json:"message"`
+}
 
+func PublicResourceIndexHandler(w http.ResponseWriter, r *http.Request) {
+	resource := Resource{Message: "public"}
+
+	json.NewEncoder(w).Encode(resource)
+}
+
+func PrivateResourceIndexHandler(w http.ResponseWriter, r *http.Request) {
+	resource := Resource{Message: "private"}
+
+	json.NewEncoder(w).Encode(resource)
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/public-resources", PublicResourceIndexHandler)
+	r.HandleFunc("/private-resources", PrivateResourceIndexHandler)
+
+	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
